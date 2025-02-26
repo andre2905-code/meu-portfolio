1325a720e515a9e54566b82bf6f9fcb75a582c1b
andre2905-code <andresreis.2018@gmail.com>
2025-02-26 16:43:13 -0300

    feat: add Contact list with each URL

--- src/components/sections/Contact/Contact.tsx
+++ src/components/sections/Contact/Contact.tsx
@@ -1,8 +1,11 @@
+import ContactsList from "./ContactsList"
+
 function Contact() {
   return (
     <div className="bg-dark-600">
-      <section className="max-w -screen-xl mx-auto p-8 text-white">
-        <h1 className="text-center text-2xl mb-4 text-white font-bold">Contato</h1>
+      <section className="max-w-screen-xl mx-auto p-8 text-white">
+        <h1 className="text-center text-2xl mb-8 text-white font-bold">Contato</h1>
+        <ContactsList />
       </section>
     </div>
   )
---
+++ src/components/sections/Contact/ContactsList.tsx
@@ -0,0 +1,45 @@
+import { FaGithub, FaLinkedin, FaPhone } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+
+const contacts = [
+  {
+    title: "Email",
+    Icon: MdEmail,
+    value: "andresreis.2018@gmail.com",
+    link: "mailto:andresreis.2018@gmail.com"
+  },
+  {
+    title: "Telefone",
+    Icon: FaPhone,
+    value: "(+55) 11 94948-9591",
+    link: "https://wa.me/5511949489591"
+  },
+  {
+    title: "LinkedIn",
+    Icon: FaLinkedin,
+    value: "/andrereis29",
+    link: "https://www.linkedin.com/in/andrereis29"
+  },
+  {
+    title: "Github",
+    Icon: FaGithub,
+    value: "@andre2905-code",
+    link: "https://github.com/andre2905-code"
+  }
+]
+
+function ContactsList() {
+  return (
+    <div className="flex justify-center gap-14">
+      {contacts.map((contact) => (
+        <a href={ contact.link } key={contact.title} target="_blank" className="flex flex-col items-center gap-2">
+          <contact.Icon size={ 28 } color="#1EB720" />
+          <strong>{contact.title}</strong>
+          <span>{ contact.value }</span>
+        </a>
+      ))}
+    </div>
+  );
+};
+
+export default ContactsList;
